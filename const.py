af9cce86d905f0200a3c9c5a46383fbaee0dadd4
coxevan <coxevan90@gmail.com>
2015-11-27 10:03:21 +0300

    File preference const
    Added file preference const value. Changing this value changes the way
    loading is handled and whether the script prefers to load the file_path
    or use the passed in data if both are supplied.

--- const.py
+++ const.py
@@ -10,7 +10,9 @@ FAILURE_THRESHOLD = 0 # How many bones can fail before user is prompted with war
 # Slows down the processes a good bit, but displays some useful information in regards to actual weighting without 
 # having to save out the files and parse. Lots of printing. Probably needs to be handled better/spit out easier to read info, but for
 # the purposes of the tool, it works as it is now.
-DEBUG = False 
+DEBUG = False
+
+FILE_PREFERENCE = True # We prefer to use the file passed in if both data and filepath are passed in during initialzation of the script.
 
 # Should the weighting be normalized? Really, this shouldn't be ever changed, but if the situation arises, this is here.
 NORMALIZE = True
